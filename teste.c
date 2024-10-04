f07963aa56b1ab6b2b42b25d1cb962d2eca3b177
bentoluizv <bentoluizv@gmail.com>
2024-10-04 11:04:24 -0300

    replace test.c original content

--- teste.c
+++ teste.c
@@ -12,25 +12,15 @@ void imprime(Requisicao *requisicao) {
 int main() {
     char nomes[][40] = {"Joao", "Jose", "Maria", "Silvia", "Sonia", "Pedro", "Lucas", "Bernardete", "Lucia", "Olivia"};
     char procedimentos[][20] = {"123.1", "345.6", "678.9", "098.1", "758.5", "23.4", "5.98", "4.32", "3.3", "1.4"};
-
     Estrutura *estrutura = create();
     int i;
     Requisicao *requisicao;
-
     for(i = 0; i < 10; i++) {
         requisicao = cria_requisicao(nomes[i], i, procedimentos[i]);
-
-        if (requisicao == NULL) {
-        printf("Erro ao criar requisicao %s\n", nomes[i]);
-        exit(1);
-
-    }
         inserir(estrutura, requisicao);
         printf("Insercao de %40s, quantidade na estrutura: %04d\n", get_nome(requisicao), get_size(estrutura));
     }
-
     printf("Fim da insercao.\nInicio da remocao\n");
-
     for(i = 0; i < 10; i++) {
         Requisicao *r = remover(estrutura);
         printf("Removido    %40s, quantidade na estrutura: %04d\n", get_nome(r), get_size(estrutura));
